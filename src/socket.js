476b9793d2fe6a6083ff7eea147b0d95e0301257
Likhith K R <151643676+Likhith-169@users.noreply.github.com>
2025-07-31 14:19:14 -0400

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 // socket.js
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000"); // Only once, not inside a function
+const socket = io("https://chat-backend-ckxr.onrender.com"); // Only once, not inside a function
 export default socket;
